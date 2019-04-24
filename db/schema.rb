8abc77770bb7f9de086f82aff19ea273cfc16e53
amnavore <abhatti1@hawk.iit.edu>
2019-04-24 17:07:17 -0500

    Generate User model
    $ rails generate model User name:string email:string uid:string

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_20_034511) do
+ActiveRecord::Schema.define(version: 2019_04_24_220634) do
 
   create_table "moods", force: :cascade do |t|
     t.string "name"
@@ -34,6 +34,14 @@ ActiveRecord::Schema.define(version: 2019_04_20_034511) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["mood_id"], name: "index_restaurants_on_mood_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "uid"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
