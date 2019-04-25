f0755af259680ccab2af2a1802265b317478a7ba
amnavore <abhatti1@hawk.iit.edu>
2019-04-24 23:43:53 -0500

    Add methods for checking signed in state

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  def user_signed_in?
+    !!current_user
+  end
+
+  def authenticate_user
+    unless user_signed_in?
+      redirect_to login_url, notice: "Please log in first"
+    end
+  end
 end
