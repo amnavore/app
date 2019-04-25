7ee03f095595ec15e716bd501a68c136b82c6c85
makk <mkahack@hawk.iit.edu>
2019-04-24 23:07:38 -0500

    Using github hash to create a new user or find user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,11 @@
 class SessionsController < ApplicationController
   protect_from_forgery except: :create
   def create
+    github_hash = request.env['omniauth.auth']
+    @tmp = request.env['omniauth.auth']
+    @user = User.find_or_create_by(uid: github_hash['uid']) do |user|
+      user.name = github_hash['info']['nickname']
+      user.email = github_hash['info']['email']
+    end
   end
 end
