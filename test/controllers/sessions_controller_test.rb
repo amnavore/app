246fc3c4248f13a4f9cf3ed5e244b179651b1f0f
makk <mkahack@hawk.iit.edu>
2019-04-25 13:01:34 -0500

    redirect to restaurants index and test

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class SessionsControllerTest < ActionDispatch::IntegrationTest
   test "should get create" do
     post '/auth/github/callback', params: OmniAuth.config.mock_auth[:github]
-    assert_response :success
+    assert_redirected_to restaurants_url
   end
 
 end
