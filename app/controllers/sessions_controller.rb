43b4072f6eb17030a523744b8e64f731c7ea6115
makk <mkahack@hawk.iit.edu>
2019-04-24 23:28:54 -0500

    Setting current user when you login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,11 +2,11 @@ class SessionsController < ApplicationController
   protect_from_forgery except: :create
   def create
     github_hash = request.env['omniauth.auth']
-    @tmp = request.env['omniauth.auth']
     @user = User.find_or_create_by(uid: github_hash['uid']) do |user|
       user.name = github_hash['info']['nickname']
       user.email = github_hash['info']['email']
     end
+    self.current_user = @user
   end
   def login
   end
