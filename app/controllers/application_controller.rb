fbb1b3fc88ac6e2a92eed257afd85655eac30229
makk <mkahack@hawk.iit.edu>
2019-04-25 00:30:10 -0500

    fixing syntax error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,14 @@ helper_method :current_user
 
 private
 
-def current_user
-  @current_user ||= User.find_by(uid: session[:uid])
-end
+  def current_user
+    @current_user ||= User.find_by(uid: session[:uid])
+  end
 
-def current_user=(user)
-  session[:uid] = user.uid
-  session[:name] = user.name
+  def current_user=(user)
+    session[:uid] = user.uid
+    session[:name] = user.name
+  end
 
   def user_signed_in?
     !!current_user
